8f620e3cc98db39aa040efd11f88abc5365b1e3c
Paless Domonkos <palessdomi@gmail.com>
2023-03-12 19:41:15 +0100

    printing looks better

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,26 @@ fn main() {
     let mut n:u128 = 5;
 
     loop {
-        let p = PrimeFactors::from((2*(n))-1).to_factor_vec().first().unwrap().integer;
-        println!("n: {}\tvalue: {}\tnext value: {}\tstep: {}", n, prev, p, (p-1)/2);
+        let p = PrimeFactors::from(((2*(n))-1) as u128).to_factor_vec().first().unwrap().integer;
+        
+        //decimal representation
+        println!("n: {}\tvalue: {}\tnext value: {}\tstep: {}", 
+        n, 
+        prev,
+        p, 
+        (p-1)/2);
+
+        // hex representation
+        /* 
+        println!("n: 0x{}\tvalue: 0x{}\tnext value: 0x{}\tstep: 0x{}", 
+        hex::encode_upper(n.to_be_bytes()).replace('0', ""), 
+        hex::encode_upper(prev.to_be_bytes()).replace('0', ""), 
+        hex::encode_upper(p.to_be_bytes()).replace('0', ""),
+        hex::encode_upper(((p-1)/2).to_be_bytes()).replace('0', "")); 
+        */
 
         prev = p;
-        if n > 100000000000000 { 
+        if n > std::u64::MAX as u128 { 
             break;
         }
         n += (p-1)/2;
